d527bc0e49497eab4790e0fc54a80b4241982249
ndwPC <magma0409@gmail.com>
2025-11-19 21:08:02 +0900

    feat: 자동차 이름이 1 ~ 5자가 아닐 때

--- src/main/kotlin/RacingCar.kt
+++ src/main/kotlin/RacingCar.kt
@@ -1,7 +1,9 @@
 package racingcar
 
-class RacingCar( private val numbers: List<String> ) {
+class RacingCar(private val numbers: List<String>) {
     init {
-
+        require(numbers.all { it.length in 1..5 }) {
+            "[ERROR] 모든 자동차 이름은 5글자 이하여야 합니다."
+        }
     }
 }
