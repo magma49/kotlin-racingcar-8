289b8a9c53ea2551ef8932a45f20704024313166
ndwPC <magma0409@gmail.com>
2025-11-19 21:12:13 +0900

    feat: 자동차 이름이 중복일 때 에러 발생하기

--- src/main/kotlin/RacingCar.kt
+++ src/main/kotlin/RacingCar.kt
@@ -5,5 +5,9 @@ class RacingCar(private val numbers: List<String>) {
         require(numbers.all { it.length in 1..5 }) {
             "[ERROR] 모든 자동차 이름은 5글자 이하여야 합니다."
         }
+
+        require(numbers.distinct().size == numbers.size) {
+            "[ERROR] 자동차 이름이 중복되면 안 됩니다."
+        }
     }
 }
