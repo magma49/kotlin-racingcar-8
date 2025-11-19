ecd05a86fa8e74055b1011ae7318c6659e5e5874
ndwPC <magma0409@gmail.com>
2025-11-19 17:30:38 +0900

    test: 자동차 이름 분리와 시도할 횟수 정수화

--- src/test/kotlin/ApplicationTest.kt
+++ src/test/kotlin/ApplicationTest.kt
@@ -12,8 +12,8 @@ internal class ApplicationTest : NsTest() {
     @Test
     fun 기능() {
         assertSimpleTest {
-            run("hi","2")
-            assertThat(output()).contains("hi","2")
+            run("pobi,woni,jun","2")
+            assertThat(output()).contains("[pobi, woni, jun]","2")
         }
     }
 
