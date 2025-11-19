b1fe9a3c81ebff6fb8681b16dce04bd428509635
ndwPC <magma0409@gmail.com>
2025-11-19 17:30:21 +0900

    feat: 자동차 이름 분리와 시도할 횟수 정수화

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -6,9 +6,12 @@ fun main() {
     println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
     val input = Console.readLine().trim()
 
+    val cars=input.split(",")
+
     println("시도할 횟수는 몇 회인가요?")
     val numstr = Console.readLine().trim()
+    val num =numstr.toInt()
 
-    println(input)
-    println(numstr)
+    println(cars)
+    println(num)
 }
