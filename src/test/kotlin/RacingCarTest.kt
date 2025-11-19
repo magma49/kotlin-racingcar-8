bd3dacd00219b99bcdc8e1d410bfe23ba0117195
ndwPC <magma0409@gmail.com>
2025-11-19 21:12:23 +0900

    test: 자동차 이름이 중복일 때 에러 발생하기

--- src/test/kotlin/RacingCarTest.kt
+++ src/test/kotlin/RacingCarTest.kt
@@ -5,9 +5,16 @@ import org.junit.jupiter.api.Test
 
 class RacingCarTest {
     @Test
-    fun 예외() {
+    fun 예외_자동차_이름이_범위를_벗어날_때() {
         assertThatThrownBy { RacingCar(listOf("magma", "magma49")) }
             .isInstanceOf(IllegalArgumentException::class.java)
             .hasMessageContaining("[ERROR] 모든 자동차 이름은 5글자 이하여야 합니다.")
+    }
+
+    @Test
+    fun 예외_자동차_이름이_중복될_때() {
+        assertThatThrownBy { RacingCar(listOf("magma", "magma")) }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessageContaining("[ERROR] 자동차 이름이 중복되면 안 됩니다.")
     }
 }
