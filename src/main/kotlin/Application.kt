75eb659727fb6bd3f5bbdd4bf95be1a77ee2322f
ndwPC <magma0409@gmail.com>
2025-11-19 17:24:53 +0900

    feat: 출력 후 입력값 받기

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -3,5 +3,12 @@ package racingcar
 import camp.nextstep.edu.missionutils.Console
 
 fun main() {
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    val input = Console.readLine().trim()
 
+    println("시도할 횟수는 몇 회인가요?")
+    val numstr = Console.readLine().trim()
+
+    println(input)
+    println(numstr)
 }
