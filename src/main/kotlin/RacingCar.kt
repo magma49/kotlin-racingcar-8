4d3e25bae8e1021f770ba8f201adda5df1a29fa7
ndwPC <magma0409@gmail.com>
2025-11-20 16:23:36 +0900

    feat: 자동차 전진시키면서 출력하기

--- src/main/kotlin/RacingCar.kt
+++ src/main/kotlin/RacingCar.kt
@@ -1,13 +1,46 @@
 package racingcar
 
-class RacingCar(private val numbers: List<String>) {
+import camp.nextstep.edu.missionutils.Randoms
+import org.assertj.core.data.Index
+
+class RacingCar(private val cars: List<String>, private val num: Int) {
+
+    private val counts: IntArray = IntArray(cars.size)
+
     init {
-        require(numbers.all { it.length in 1..5 }) {
+        require(cars.all { it.length in 1..5 }) {
             "[ERROR] 모든 자동차 이름은 5글자 이하여야 합니다."
         }
 
-        require(numbers.distinct().size == numbers.size) {
+        require(cars.distinct().size == cars.size) {
             "[ERROR] 자동차 이름이 중복되면 안 됩니다."
         }
+    }
+
+    fun race() {
+        var check = true
+        while (check) {
+            for ((index, car) in cars.withIndex()) {
+                printNow(car, counts[index])
+                check = move(index, check)
+                print("\n")
+            }
+            print("\n")
+        }
+    }
+
+    fun printNow(car: String, num: Int) {
+        print(car + " : ")
+        for (i in 1..num)
+            print("-")
+    }
+
+    fun move(index: Int, check: Boolean): Boolean {
+        if (Randoms.pickNumberInRange(0, 9) >= 4) {
+            print("-")
+            if (++counts[index] == num)
+                return false
+        }
+        return check
     }
 }
